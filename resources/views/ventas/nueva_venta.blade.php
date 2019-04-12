2597c59b6a407fd915ee1c7fdc2841a1524c2b16
luisb0992 <luisbarrios0992@gmail.com>
2019-04-12 10:01:32 -0400

    en proceso

--- resources/views/ventas/nueva_venta.blade.php
+++ resources/views/ventas/nueva_venta.blade.php
@@ -11,18 +11,24 @@
 
 	<div class="row">
   	<div class="col-lg-12">
-      <a class="btn btn-app bg-navy col-lg-5" href="{{ route('ventas.index') }}">
-        <i class="fa fa-file"></i> Consignacion
-      </a>
-      <a class="btn btn-app bg-purple col-lg-5" href="{{ route('ventas.index') }}">
-        <i class="fa fa-file-text"></i> Venta directa
-      </a>
-		</div>
-	</div>
+    	<div class="box box-danger box-solid">
+    		<div class="box-body" id="box-body">
+            <div class="col-lg-12">
+              <a class="btn btn-app bg-navy" href="#" data-toggle="tooltip" title="Nueva consignacion" id="btn_nueva_consignacion">
+                <i class="fa fa-file"></i> Consignacion
+              </a>
+              <a class="btn btn-app bg-orange" href="#" data-toggle="tooltip" title="Nueva venta" id="btn_nueva_venta">
+                <i class="fa fa-file-text"></i> Venta directa
+              </a>
+            </div> 
+	      </div>
+      </div>
+    </div>
+  </div>    
 
 @endsection
 @section("script")
 <script>
-
+  
 </script>
 @endsection
