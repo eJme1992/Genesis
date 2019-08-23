fc3d09af0c14e0a25e31966a4b2b161f04e19056
luisb0992 <luisbarrios0992@gmail.com>
2019-08-23 08:02:50 -0400

    desactivado

--- resources/views/ventas/partials/sections/section_btn_submit_volver.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group col-lg-12">
-    <hr>
-    <button type="submit" class="btn btn-success btn-flat" id="btn_guardar_all">
-        <i class="fa fa-save" id="icon-guardar-all"></i> Procesar Venta
-    </button>
-    <button type="button" class="btn btn-default btn-flat" id="btn_volver">
-        <i class="fa fa-reply"></i> Volver
-    </button>
-</div>
